c14a4404f59803d074489773d7a13abb04d4b097
Tyron William CHANU <tyronwilliam.chanu@veepee.com>
2025-05-14 23:38:44 +0200

    curriculum end accueil

--- src/app/_components/Accueil/Accueil.tsx
+++ src/app/_components/Accueil/Accueil.tsx
@@ -1,84 +1,61 @@
-import { StaticImageData } from "next/image";
-import { ReactElement, useCallback, useEffect, useState } from "react";
-import { BsBookmarkDashFill } from "react-icons/bs";
-import {
-  FaCss3Alt,
-  FaGitAlt,
-  FaHtml5,
-  FaNodeJs,
-  FaReact,
-  FaSass,
-  FaVuejs,
-} from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io";
-import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
-import { SiOvh, SiStrapi, SiTypescript } from "react-icons/si";
-import { TbSql } from "react-icons/tb";
-import { Autoplay } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Agence from "../../../../public/images/agence.png";
-import CSB from "../../../../public/images/CSB.png";
-import Coop from "../../../../public/images/lacoop.png";
-import { LeftSide } from "./LeftSide";
+import { StaticImageData } from 'next/image'
+import { ReactElement, useState } from 'react'
+import { BsBookmarkDashFill } from 'react-icons/bs'
+import { FaCss3Alt, FaGitAlt, FaHtml5, FaNodeJs, FaReact, FaSass, FaVuejs } from 'react-icons/fa'
+import { IoLogoJavascript } from 'react-icons/io'
+import { RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri'
+import { SiOvh, SiStrapi, SiTypescript } from 'react-icons/si'
+import { TbSql } from 'react-icons/tb'
+import { Autoplay } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import Agence from '../../../../public/images/agence.png'
+import CSB from '../../../../public/images/CSB.png'
+import Coop from '../../../../public/images/lacoop.png'
 
-import "swiper/css";
-import "swiper/css/effect-cards";
-import { cn } from "../../../../lib/utils/classnames";
+import 'swiper/css'
+import 'swiper/css/effect-cards'
+import { cn } from '../../../../lib/utils/classnames'
 
-type Props = {};
+type Props = {}
 
 export interface ProjectItem {
-  name: string;
-  description: string;
-  rating: string;
-  icon: ReactElement;
-  img: StaticImageData;
+  name: string
+  description: string
+  rating: string
+  icon: ReactElement
+  img: StaticImageData
 }
 const projects: ProjectItem[] = [
   {
-    name: "Agence Graziani",
-    description: "App pour une agence de casting",
-    rating: "4.9",
-    icon: (
-      <BsBookmarkDashFill className=" text-blue-900 w-[16px] h-[19px] ml-auto" />
-    ),
-    img: Agence,
+    name: 'Agence Graziani',
+    description: 'App pour une agence de casting',
+    rating: '4.9',
+    icon: <BsBookmarkDashFill className=" text-blue-900 w-[16px] h-[19px] ml-auto" />,
+    img: Agence
   },
   {
-    name: "CSB Klinik",
+    name: 'CSB Klinik',
     description: "Site d'actualité & e-commerce",
-    rating: "5",
-    icon: (
-      <BsBookmarkDashFill className=" text-blue-900 w-[16px] h-[19px] ml-auto" />
-    ),
-    img: CSB,
+    rating: '5',
+    icon: <BsBookmarkDashFill className=" text-blue-900 w-[16px] h-[19px] ml-auto" />,
+    img: CSB
   },
   {
-    name: "LA COOP",
-    description: "Site vitrine pour un bar",
-    rating: "5",
-    icon: (
-      <BsBookmarkDashFill className=" text-blue-900 w-[16px] h-[19px] ml-auto" />
-    ),
-    img: Coop,
-  },
-];
+    name: 'LA COOP',
+    description: 'Site vitrine pour un bar',
+    rating: '5',
+    icon: <BsBookmarkDashFill className=" text-blue-900 w-[16px] h-[19px] ml-auto" />,
+    img: Coop
+  }
+]
 export const arsenal = [
   <RiTailwindCssFill
     className="w-full h-full text-[#38BDF8]"
     aria-label="Tailwind CSS"
     title="Tailwind CSS"
   />,
-  <FaHtml5
-    className="w-full h-full text-[#E34F26]"
-    aria-label="HTML5"
-    title="HTML5"
-  />,
-  <FaCss3Alt
-    className="w-full h-full text-[#1572B6]"
-    aria-label="CSS3"
-    title="CSS3"
-  />,
+  <FaHtml5 className="w-full h-full text-[#E34F26]" aria-label="HTML5" title="HTML5" />,
+  <FaCss3Alt className="w-full h-full text-[#1572B6]" aria-label="CSS3" title="CSS3" />,
   <IoLogoJavascript
     className="w-full h-full text-[#F7DF1E]"
     aria-label="JavaScript"
@@ -89,133 +66,99 @@ export const arsenal = [
     aria-label="TypeScript"
     title="TypeScript"
   />,
-  <FaSass
-    className="w-full h-full text-[#CC6699]"
-    aria-label="Sass"
-    title="Sass"
-  />,
-  <RiNextjsFill
-    className="w-full h-full"
-    aria-label="Next.js"
-    title="Next.js"
-  />,
-  <FaReact
-    className="w-full h-full text-[#61DAFB]"
-    aria-label="React"
-    title="React"
-  />,
-  <FaVuejs
-    className="w-full h-full text-[#42B883]"
-    aria-label="Vue.js"
-    title="Vue.js"
-  />,
-  <FaGitAlt
-    className="w-full h-full text-[#F05032]"
-    aria-label="Git"
-    title="Git"
-  />,
-  <FaNodeJs
-    className="w-full h-full text-[#339933]"
-    aria-label="Node.js"
-    title="Node.js"
-  />,
-  <TbSql
-    className="w-full h-full text-[#003B57]"
-    aria-label="SQL"
-    title="SQL"
-  />,
-  <SiStrapi
-    className="w-full h-full text-[#003B57]"
-    aria-label="Strapi"
-    title="Strapi"
-  />,
-  <SiOvh
-    className="w-full h-full text-[#003B57]"
-    aria-label="OVH"
-    title="OVH"
-  />,
-];
-const name = "Tyron William";
-const job = "Développeur Front-end";
+  <FaSass className="w-full h-full text-[#CC6699]" aria-label="Sass" title="Sass" />,
+  <RiNextjsFill className="w-full h-full" aria-label="Next.js" title="Next.js" />,
+  <FaReact className="w-full h-full text-[#61DAFB]" aria-label="React" title="React" />,
+  <FaVuejs className="w-full h-full text-[#42B883]" aria-label="Vue.js" title="Vue.js" />,
+  <FaGitAlt className="w-full h-full text-[#F05032]" aria-label="Git" title="Git" />,
+  <FaNodeJs className="w-full h-full text-[#339933]" aria-label="Node.js" title="Node.js" />,
+  <TbSql className="w-full h-full text-[#003B57]" aria-label="SQL" title="SQL" />,
+  <SiStrapi className="w-full h-full text-[#003B57]" aria-label="Strapi" title="Strapi" />,
+  <SiOvh className="w-full h-full text-[#003B57]" aria-label="OVH" title="OVH" />
+]
+const name = 'Tyron-William'
+
+const arraySquare = [['Tyron'], [''], ['William']]
+const profession = 'Développeur Front-end'
 
 export const Accueil = ({}: Props) => {
-  const [show, setShow] = useState<Boolean>(false);
+  const [show, setShow] = useState<Boolean>(false)
 
   return (
     <section className="w-full h-full relative flex-1 flex flex-col gap-10 md:gap-0 lg:p-4 lg:pt-0 grow ">
       {/* <LeftSide /> */}
-      <div className="container_name">
-        <h1 className="name_title">
-          {[...name].map((letter, i) => (
-            <span
-              key={`outline-${i}`}
-              className={cn(show ? "show" : "hide")}
-              style={{ animationDelay: `${i * 0.1}s` }}
-            >
-              {" "}
-              <div className="glassomorph_effect p-2 flex justify-center items-center">
-                {letter !== " " && letter}
-              </div>
-            </span>
-          ))}
-        </h1>
-      </div>
-      <div className="container_name">
-        <h1 className="name_title">
-          {[...job].map((letter, i) => (
-            <span
-              key={`outline-${i}`}
-              className={cn(show ? "show" : "hide")}
-              style={{ animationDelay: `${i * 0.1}s` }}
-            >
-              {" "}
-              <div className="glassomorph_effect p-2 flex justify-center items-center">
-                {letter !== " " && letter}
-              </div>
-            </span>
-          ))}
-        </h1>
-      </div>
+      <h1 className="name_title flex flex-col items-center gap-2">
+        {arraySquare.map((group, groupIndex) => (
+          <div key={`group-${groupIndex}`} className="flex justify-center w-full">
+            {[...group[0]].map((letter, letterIndex) => (
+              <span
+                key={`letter-${groupIndex}-${letterIndex}`}
+                className={cn(show ? 'show' : 'hide')}
+                style={{ animationDelay: `${(groupIndex * 5 + letterIndex) * 0.1}s` }}
+              >
+                <div className="glassomorph_effect p-2 flex justify-center items-center">
+                  {letter !== ' ' && letter}
+                </div>
+              </span>
+            ))}
+          </div>
+        ))}
+      </h1>
 
+      <div className="container_profession">
+        <h1 className="name_profession ">
+          {[...profession].map((letter, i) => (
+            <span
+              key={`outline-${i}`}
+              className={cn(show ? 'show' : 'hide')}
+              style={{ animationDelay: `${2.6 + i * 0.1}s` }}
+            >
+              {' '}
+              <div className="glassomorph_effect p-2 flex justify-center items-center">
+                {letter !== '' && letter}
+              </div>
+            </span>
+          ))}
+        </h1>
+      </div>
       <div
         onClick={() => {
-          setShow(!show);
+          setShow(!show)
         }}
-        className="w-12 h-12 rounded-full absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex justify-center items-center glassomorph_effect cursor-pointer z-50 "
+        className={cn(
+          'w-12 h-12 rounded-full absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex justify-center items-center glassomorph_effect cursor-pointer z-50 ',
+          show && 'hide'
+        )}
       >
         <span className="absolute inline-flex h-1/2 w-1/2 animate-ping rounded-full bg-red-900 opacity-75"></span>
         <span className="relative inline-flex  w-1/2 h-1/2  rounded-full bg-red-900"></span>
       </div>
     </section>
-  );
-};
+  )
+}
 
 const BottomSection = () => {
   return (
     <>
       <LeftBottom />
-      <div className="w-full h-full glassomorph_effect_bis p-4">
-        Veeppee current workplace
-      </div>
+      <div className="w-full h-full glassomorph_effect_bis p-4">Veeppee current workplace</div>
     </>
-  );
-};
+  )
+}
 const LeftBottom = () => {
   return (
     <div className="glassomorph_effect_bis w-[70%]  xl:w-[65%] p-4 lg:h-full ">
       {/* TItle */}
-      <h3 className="tracking-wider text-2xl font-bold  mb-2 xl:mb-8">
-        Techno
-      </h3>
+      <h3 className="tracking-wider text-2xl font-bold  mb-2 xl:mb-8">Techno</h3>
       {/* Carousel */}
       <Swiper
         breakpoints={{
           0: {
-            slidesPerView: 1,
+            slidesPerView: 1
           },
           768: {
-            slidesPerView: 3,
-          },
+            slidesPerView: 3
+          }
         }}
         spaceBetween={20}
         loop={true}
@@ -223,7 +166,7 @@ const LeftBottom = () => {
         speed={5000}
         autoplay={{
           delay: 0,
-          disableOnInteraction: false,
+          disableOnInteraction: false
         }}
         grabCursor={false}
         modules={[Autoplay]}
@@ -234,7 +177,7 @@ const LeftBottom = () => {
           <SwiperSlide
             key={index}
             className={cn(
-              "w-48 max-w-[95%] flex items-center justify-center",
+              'w-48 max-w-[95%] flex items-center justify-center',
               `slide-index-${index + 1}`
             )}
           >
@@ -243,5 +186,5 @@ const LeftBottom = () => {
         ))}
       </Swiper>
     </div>
-  );
-};
+  )
+}
--- src/app/_components/Navigation/TabView.tsx
+++ src/app/_components/Navigation/TabView.tsx
@@ -1,39 +1,50 @@
-import { NavigationLabel } from "@/app/hooks/useNavigation";
-import { Accueil } from "../Accueil/Accueil";
-import { Apropos } from "../Apropos/Apropos";
-import { Projets } from "../Projets/Projets";
-import React from "react";
-import { cn } from "../../../../lib/utils/classnames";
+import { NavigationLabel } from '@/app/hooks/useNavigation'
+import { Accueil } from '../Accueil/Accueil'
+import { Apropos } from '../Apropos/Apropos'
+import { Projets } from '../Projets/Projets'
+import React from 'react'
+import { cn } from '../../../../lib/utils/classnames'
+import { Curriculum } from '../Curriculum/Curriculum'
 
 type Props = {
-  selectedTab: NavigationLabel;
-  direction: string;
-};
+  selectedTab: NavigationLabel
+  direction: string
+}
 export const TabViews = ({ selectedTab, direction }: Props) => {
   return (
     <section className="w-full h-[calc(100%-40px)] shrink-0 p-3">
       <div
-        className={`${selectedTab !== NavigationLabel.Accueil ? "exit-right-animation" : "enter-animation"}`}
+        className={`${selectedTab !== NavigationLabel.Accueil ? 'exit-right-animation' : 'enter-animation'}`}
       >
         <Accueil />
       </div>
-
       <div
         className={cn(
           selectedTab === NavigationLabel.APropos
-            ? "enter-animation"
-            : direction === "right"
-              ? "exit-right-animation"
-              : "exit-left-animation"
+            ? 'enter-animation'
+            : direction === 'right'
+              ? 'exit-right-animation'
+              : 'exit-left-animation'
         )}
       >
         <Apropos />
       </div>
       <div
-        className={`${selectedTab !== NavigationLabel.Projets ? "exit-bottom-animation" : "enter-top-animation"}`}
+        className={`${selectedTab !== NavigationLabel.Projets ? 'exit-bottom-animation' : 'enter-top-animation'}`}
       >
         <Projets />
+      </div>{' '}
+      <div
+        className={cn(
+          selectedTab === NavigationLabel.Curriculum
+            ? 'enter-animation'
+            : direction === 'right'
+              ? 'exit-right-animation'
+              : 'exit-left-animation'
+        )}
+      >
+        <Curriculum />
       </div>
     </section>
-  );
-};
+  )
+}
